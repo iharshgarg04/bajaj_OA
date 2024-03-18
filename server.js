0231701c8de689b92ed46c5c5d3eb6c9997f5d9d
Harsh Garg <143173757+iharshgarg04@users.noreply.github.com>
2024-03-18 07:57:13 +0000

    corrected errors

--- server.js
+++ server.js
@@ -1,42 +1,38 @@
-const express = require('express');
-const cors = require('cors');
-
-
-const app = express();
-app.use(express.json());
-const port = 4000;
+const express = require("express");
+const cors = require("cors");
+const PORT = 4000;
+const  app = express();
 app.use(cors({
-    origin: '*',
-    credentials: true
-}));
+    origin:"*",
+    credentials:true
+}))
+app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-
-app.post('/bfhl',(req,res)=>{
+app.post('/bfhl',async(req,res)=>{
     try{
-    console.log(req.body)
-    const {data}=req.body;
-    const even=data.filter((num)=>typeof parseInt(num)==='number' && parseInt(num)%2===0);
-    const odd=data.filter((num)=>typeof parseInt(num)==='number' && parseInt(num)%2===1);
-    const alpha=data.filter((num)=>typeof num==='string' && isNaN(parseInt(num)));
-    const upper=alpha.map((al)=>al.toUpperCase());
-    res.json({
-        is_success: true,
-        user_id: "vivek_dhiman_01062003",
-        email : "vivek1552.be21@chitkara.edu.in",
-        roll_number:2110991552,
-        odd_numbers: odd,
-        even_numbers: even,
-        alphabets: upper
-    }).status(200);}
-    catch(err){
-        res.json({is_success: false,}).status(400);
-}
+        const {data}=req.body;
+        const even= data.filter((num)=>typeof parseInt(num)==='number' && parseInt(num)%2===0);
+        const odd= data.filter((num)=>typeof parseInt(num)==='number' && parseInt(num)%2===1);
+        const alpha= data.filter((num)=>typeof num==='string' && isNaN(parseInt(num)));
+        const upper=alpha.map((al)=>al.toUpperCase());
+        const user_id = "harsh1713";
+        const email = "harsh1713.be21@chitkara.edu.in";
+        const roll_no = "2110991713";
+        res.status(200).json({
+            is_success:true,
+            user_id : user_id,
+            roll_number: roll_no,
+            odd_numbers : even ,
+            even_numbers: odd_numbers,
+            alphabets : upper
+        })
+    }catch(error){
+        console.log(error);
+    }
 })
 
-
-
-app.listen(port, () => {
-    console.log(`Server listening at ${port}`);
-});
+app.listen(PORT,()=>{
+    console.log(`server is successfully running on port ${PORT}`);
+})
